e3df9bde7ffdfbac519519cc79784b58a7894b54
Carlos Hernan <86528810+Hernan-DOS@users.noreply.github.com>
2023-11-10 20:21:41 -0300

    Delete Laboratorio IV/Javascript/Cuarta semana de septiembre directory

--- Laboratorio IV/Javascript/Cuarta semana de septiembre/PERN-STACK/database/init.sql
+++
@@ -1,6 +0,0 @@
--- Este archivo hace referencia a la base de datos o al script sql que vamos a utilizar
-CREATE TABLE tareas (
-    id SERIAL PRIMARY KEY,
-    titulo VARCHAR(255) UNIQUE NOT NULL,
-    descripcion TEXT,
-);
